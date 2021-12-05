ca538dc1deee65f9754f276af0dbe30b80232cfe
irawild <irawildjunior@gmail.com>
2021-12-05 02:12:48 -0300

    Start managing state on MyComponent

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 const MyComponent = (props) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    setUser({ name: "Irawild", age: 46 });
+    return () => {};
+  }, []);
+
   return (
     <div>
-      <h2>This is my first component</h2>
+      <h2>This is my first component, {user && user.name} </h2>
     </div>
   );
 };
 
-export default MyComponent
+export default MyComponent;
